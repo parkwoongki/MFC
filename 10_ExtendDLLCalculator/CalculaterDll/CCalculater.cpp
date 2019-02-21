502488513d0e6a0acd40e3156bd9668249d635ac
parkwoongki <sbe03005@naver.com>
2019-02-21 22:27:12 +0900

    Calculator By Extend DLL

---
+++ 10_ExtendDLLCalculator/CalculaterDll/CCalculater.cpp
@@ -0,0 +1,33 @@
+#include "stdafx.h"
+#include "CCalculater.h"
+
+
+CCalculater::CCalculater()
+{
+}
+
+
+CCalculater::~CCalculater()
+{
+}
+
+
+double CCalculater::Add(double a, double b) {
+	return a + b;
+}
+
+
+double CCalculater::Sub(double a, double b) {
+	return a - b;
+}
+
+
+double CCalculater::Mul(double a, double b) {
+	return a * b;
+}
+
+
+double CCalculater::Div(double a, double b) {
+	return a / b;
+}
+
---
+++ 10_ExtendDLLCalculator/CalculaterDll/CCalculater.h
@@ -0,0 +1,12 @@
+#pragma once
+class AFX_EXT_CLASS CCalculater
+{
+public:
+	CCalculater();
+	~CCalculater();
+	double CCalculater::Add(double a, double b);
+	double CCalculater::Sub(double a, double b);
+	double CCalculater::Mul(double a, double b);
+	double CCalculater::Div(double a, double b);
+};
+
